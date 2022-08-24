5a3bb86b237c50c28922bdaee734e302595a8cd2
unknown <zayatsunami@gmail.com>
2022-08-23 13:14:45 -1200

    Done

---
+++ main.js
@@ -0,0 +1,40 @@
+function fauteuils (){
+ $(".row").hide()
+ $('.slideshow-container').hide()
+ $(".fauteuils").show() 
+ $(".btn").show()
+}
+
+function luminaires(){
+  $(".row").hide()
+ $('.slideshow-container').hide()
+ $(".luminaires").show() 
+ $(".btn").show()
+}
+
+function salleAm(){
+    $(".row").hide()
+ $('.slideshow-container').hide()
+ $(".salleAm").show()
+ $(".btn").show()
+
+}
+
+function lits(){
+    $(".row").hide()
+ $('.slideshow-container').hide()
+ $(".lits").show() 
+ $(".btn").show()
+
+}
+
+function back(){
+  $(".row").show()
+ $('.slideshow-container').show()
+ $(".lits").hide()
+ $(".salleAm").hide()
+ $(".luminaires").hide() 
+ $(".fauteuils").hide() 
+  $(".btn").hide()
+
+}
